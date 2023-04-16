a537a5a0a2c52d3c0190e4a63c643cfb9d3c07b3
Lucas Maciel <lucasmaciel63@gmail.com>
2023-04-16 17:02:00 -0300

    Adding recipes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecepeListComponent } from './recipes/recipe-list/recepe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecepeListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipe-list/recepe-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recepe-list',
+  templateUrl: './recepe-list.component.html',
+  styleUrls: ['./recepe-list.component.css']
+})
+export class RecepeListComponent {
+
+}
