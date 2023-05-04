5ccf4ffaebee86a1650426d156de78cc3b752d3b
Lucas Maciel <lucasmaciel63@gmail.com>
2023-05-03 21:49:55 -0300

    Add models and some examples to the templates

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecepeListComponent {
+export class RecepeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe 1', 'Is a test 1', 'https://assets.telegraphindia.com/telegraph/2022/Dec/1670922198_shutterstock_2146741411-1.jpg'),
+    new Recipe('Test Recipe 1', 'Is a test 1', 'https://assets.telegraphindia.com/telegraph/2022/Dec/1670922198_shutterstock_2146741411-1.jpg'),
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
 
 }
